c17dddaa4cd83ba21e2efac6f31e66c5e2629458
Sivarajarajeshwaran <148957149+Sivarajarajeshwaran@users.noreply.github.com>
2026-06-01 10:13:52 +0530

    1

--- read_pdf.js
+++ read_pdf.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const PDFParser = require("pdf2json");
 
-const pdfParser = new PDFParser(this, 1);
+const pdfParser = new PDFParser(null, 1);
 
 pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
 pdfParser.on("pdfParser_dataReady", pdfData => {
